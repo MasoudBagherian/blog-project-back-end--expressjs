5b8ff1a865d3564e8ea4d831e2d35f6b5af0464f
Pouria Baqerian <pouriacss.69@gmail.com>
2021-09-02 03:39:56 +0430

    add User model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'blogger'],
+    default: 'blogger'
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+
+  }
+})
+
+module.exports = mongoose.model('users', userSchema)
